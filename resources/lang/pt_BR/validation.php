d90d5711d7fb0bb68d8281e840cc0db41021ad9d
anaahnb <alvncar@gmail.com>
2026-01-09 19:55:15 -0300

    feat: add errors validation in register form

---
+++ resources/lang/pt_BR/validation.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+  'required' => 'O campo :attribute é obrigatório.',
+  'email' => 'Informe um endereço de e-mail válido.',
+  'min' => [
+    'string' => 'O campo :attribute deve ter no mínimo :min caracteres.',
+  ],
+  'confirmed' => 'A confirmação de :attribute não confere.',
+  'unique' => 'Este :attribute já está em uso.',
+
+  'attributes' => [
+    'name' => 'nome',
+    'email' => 'e-mail',
+    'password' => 'senha',
+  ],
+
+];
